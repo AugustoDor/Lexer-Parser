278b26c816db0883974210e55b0f944c12332baf
Augusto Dor <augustodor1234@gmail.com>
2024-05-30 10:36:55 -0300

    ejemplo lexer

---
+++ python/Json/main.py
@@ -0,0 +1,36 @@
+"""
+EJEMPLO:
+{
+"empresas": [
+{
+"nombre_empresa": "Mc Donalds",
+"fundación": 2005,
+"dirección": null,
+"ingresos_anuales": 200000.25,
+"pyme": false,
+"departamentos": [
+{
+"nombre": "Ventas",
+"subdepartamentos": [
+{
+"nombre": "Mc Donalds JUC",
+"empleados": [
+{
+"nombre": "Sideshow Mel",
+"cargo": "Product Analyst",
+"salario": 1250.65,
+"activo": true,
+"fecha_contratación": "2023-09-10",
+"proyectos": [
+{
+"nombre": "Mc Flurry",
+"fecha_inicio": "2022-01-10",
+"fecha_fin": null
+} ]
+} ]
+} ]
+} ]
+}
+]
+}
+"""
